52ce363ddb4ca70bb6f48b2112f46c24ffdf9908
Lukas Sarfert <lukassarfert@googlemail.com>
2024-06-28 12:04:02 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,11 @@ ALERT_DAY=${ALERT_DAY:-Friday}
 ALERT_TIME=${ALERT_TIME:-12:05}
 ALERT_DURATION=${ALERT_DURATION:-5}
 
-# Prepare the cron job string
+# Prepare the cron job string for start time
 START_TIME="${ALERT_TIME:3:2} ${ALERT_TIME:0:2} * * $(date -d "$ALERT_DAY" +%u)"
-END_TIME=$(date -d "$ALERT_DAY $ALERT_TIME + $ALERT_DURATION minutes" + "%M %H * * $(date -d "$ALERT_DAY" +%u)")
+
+# Calculate the end time for the cron job
+END_TIME=$(date -d "$ALERT_TIME today + $ALERT_DURATION minutes" +"%M %H * * $(date -d $ALERT_DAY +%u)")
 
 # Add cron job to switch alert on
 echo "$START_TIME root /usr/local/bin/toggle_alert.sh start" > /etc/cron.d/probealarm-cron
